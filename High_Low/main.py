4d38f04790c9f282204167c58f208dbb96c816ce
sarthaksharma660 <sarthaksharma600@gmail.com>
2021-01-17 10:43:18 +0530

    final touches

--- High_Low/main.py
+++ High_Low/main.py
@@ -1,8 +1,8 @@
 from art_High_Low import logo,vs
 from game_data import data
 import random
+import os
 score=0
-print(logo)
 game_over=True
 a_dic={
     'name': "sarthak",
@@ -31,16 +31,19 @@ def winner(Input):
     global a_dic,b_dic
     global score
     if (a_dic['follower_count']>b_dic['follower_count'] and Input=="a") or(a_dic['follower_count']<b_dic['follower_count'] and Input=="b") :
+        os.system("clear")
         score=score+1
         a_dic=b_dic
         print(f"You're right! Current score: {score}.")
         return score
     else:
+        os.system("clear")
         print(f"Sorry, that's wrong. Final score: {score}")
         score=0
         return score
 a_dic=randomvalue()
 while game_over:
+    print(logo)
     print(f"Compare A:{a_dic['name']},{a_dic['description']},from {a_dic['country']}.")
     print(vs)
     b_dic=randomvalue()
